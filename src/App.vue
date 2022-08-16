9666ef6fd7fb2dddc508413cfe2a851840a0ce87
aaaddf <eee@qq.com>
2022-08-16 19:26:41 +0800

    添加todo

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <section class="todoapp">
     <header class="header">
       <h1>Todos</h1>
-      <input type="text" class="new-todo">
+      <input type="text" class="new-todo" v-model="newTodo" @keyup.enter="addTodo">
     </header>
     <section class="main">
-        <input type="checkbox" id="toggle-all" class="toggle-all">
-        <label for="toggle-all"></label>
-        <ul class="todo-list">
-             <todo-item></todo-item>
-             <todo-item></todo-item>
-             <todo-item></todo-item>
-        </ul>
-      </section>
-     <todo-footer></todo-footer>
+      <input type="checkbox" id="toggle-all" class="toggle-all">
+      <label for="toggle-all"></label>
+      <ul class="todo-list">
+        <todo-item></todo-item>
+        <todo-item></todo-item>
+        <todo-item></todo-item>
+      </ul>
+    </section>
+    <todo-footer></todo-footer>
   </section>
 </template>
 
@@ -21,11 +21,28 @@
 import TodoItem from "@/components/TodoItem";
 import TodoFooter from "@/components/TodoFooter";
 export default {
-  name:"App",
-  components: {TodoFooter, TodoItem}
+  name: "App",
+  components: { TodoFooter, TodoItem },
+  data() {
+    return {
+      newTodo: '',
+      todoDatas: []
+    }
+  },
+  methods: {
+    addTodo() {
+      if(this.newTodo==='')return
+      let todo={}
+      todo.id=Date.now();
+      todo.value=this.newTodo.trim();
+      todo.hasCompleted=false;
+      this.todoDatas.push(todo);
+      this.newTodo='';
+      console.log(this.todoDatas,'this.todoDatas');
+    }
+  },
 }
 </script>
 
 <style scoped>
-
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,23 +1,22 @@
 <template>
-   <li>
-     <div class="view">
-       <input type="checkbox" class="toggle">
-       <label>Content</label>
-       <button class="destroy"></button>
-     </div>
-     <input type="text" class="edit">
-   </li>
+  <li>
+    <div class="view">
+      <input type="checkbox" class="toggle">
+      <label>Content</label>
+      <button class="destroy"></button>
+    </div>
+    <input type="text" class="edit">
+  </li>
 </template>
 
 <script>
 export default {
   name: "TodoItem",
   mounted() {
-    console.log("this",this);
+    console.log("this", this);
   }
 }
 </script>
 
 <style scoped>
-
 </style>
